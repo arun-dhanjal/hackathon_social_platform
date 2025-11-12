a5e26406723d4f6bab08b86c9fab6cb0c01aa562
KoroYsabela <ysabelabathan2604@gmail.com>
2025-11-12 16:27:55 +0000

    add: events home, event detail and my events pages, user can host event and book

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,11 @@
+from . import views
 from django.urls import path
 
 app_name = 'events'
 
 urlpatterns = [
-    # Add your events URLs here
+    path('', views.EventsList.as_view(), name='events_feed'),
+    path('event/<slug:slug>/', views.event_detail, name='event_detail'),
+    path('event/<slug:slug>/book/', views.book_event, name='book_event'),
+    path('myevents/', views.my_events, name='my_events'),
 ]
