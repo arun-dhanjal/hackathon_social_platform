4031150d980a514084646a39b92b228dd455fb89
Adam Haines <adamlh2003@gmail.com>
2025-11-12 12:19:11 +0000

    Add simple view for feed

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, Comment
+
 
 # Create your views here.
+class Feed(generic.ListView):
+    template_name = "feed/feed.html"
+    context_object_name = "posts"
+    paginate_by = 6
+
+    queryset = Post.objects.all().filter(accepted=True).order_by("-created_on")
