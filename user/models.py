8e0fd907af0f0b5d275e5d1d32512c44ac4b6d55
Arun Dhanjal <arundhanjal@hotmail.com>
2025-11-13 14:10:15 +0000

    Replace image field with cloudinary field and overwrite settings.py file with previous version

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 
 class UserProfile(models.Model):
@@ -8,7 +9,7 @@ class UserProfile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture = CloudinaryField("image", null=True, blank=True)
     
     # Social media fields for the social platform
     website = models.URLField(blank=True)
