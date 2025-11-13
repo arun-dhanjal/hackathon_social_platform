86d370ac0fedcce6c7e26fe3a5e891d51a3e9af2
Mortaza Zolfpour Arokhlo <mortazazolfpour@gmail.com>
2025-11-13 11:31:36 +0000

    Romeve 2 profile folders and add user authentication templates including login, signup, profile, and password reset pages

--- profiles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'location', 'birth_date')
-    search_fields = ('user__username', 'user__email', 'location')
